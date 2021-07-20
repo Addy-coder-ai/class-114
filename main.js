c13df1ef3d0d5938523f06435df9e5e8fff1c11b
Addy-coder-ai <80392675+Addy-coder-ai@users.noreply.github.com>
2021-07-20 17:43:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//nothing to see here. if u see H@cK$r$ will com$#
+//p5.js is D#$l&t$D
+
+function preload() {
+}
+
+
+function setup() {
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function takeApic() {
+    save("Mypicture.png");
+}
+//H@h@h@H@H@H@HH@H@H@HH@H@H@HH@H@H@Ha
